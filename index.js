85e0645024cad70d43c747da085d61073faaa421
Bridgett Wheeler <bridgettwheeler@gmail.com>
2021-06-30 15:37:01 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -21,13 +21,16 @@ function distanceTravelledInFeet (beginingBlock, endBlock) {
   } 
 
 function calculatesFarePrice(beginingBlock, endBlock) {
-    return distanceTravelledInFeet;
-    if (distanceTravelledInFeet < 401) {
+    let feet = distanceTravelledInFeet(beginingBlock, endBlock);
+    console.log(feet);
+    if (feet < 401) {
         return 0;
-    } else if (distanceTravelledInFeet == 400 && 2000) {
-        return (distanceTravelledInFeet - 400)*.02;
-    } else if (distanceFromHqInFeet == 2000 && 2500) {
-        return
+    } else if (feet > 400 && feet < 2000) {
+        return (feet - 400)*.02;
+    } else if (feet >= 2000 && feet < 2500) {
+        return 25;
+    } else if (feet > 2500) {
+        return 'cannot travel that far';
     }
 
 }
